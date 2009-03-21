b7145ae5e52f3a7783d75f42866f85eb963e37b5
deepak.engg <deepak.engg@f2bf0144-0f3c-11de-9004-1fff2a6de87b>
2009-03-21 20:10:50 +0000

    Testing Commit

---
+++ trunk/IM/src/clientDetails.java
@@ -0,0 +1,16 @@
+
+public class clientDetails {
+	
+	// testing SVN
+	int a = 1;
+	int b = 2;
+	int c = a + b;
+	
+	
+	public void display()
+	{
+		System.out.println("the value of c is:" +c);
+	}
+	
+
+}
