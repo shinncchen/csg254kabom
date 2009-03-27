0bacea5cb93110fd3e06fda7ca32189e764a7f3f
swim82 <swim82@f2bf0144-0f3c-11de-9004-1fff2a6de87b>
2009-03-27 22:52:33 +0000

    [Abdulla] SHA1, Timestamps are implemented

---
+++ trunk/IM/src/client/security/SHA1.java
@@ -0,0 +1,24 @@
+/**
+ * This class is used to calculate message digest using SHA1
+ * Team members don't call this class. Use Security() instead.
+ */
+package client.security;
+
+import java.security.*;
+/**
+ * @author Abdulla
+ *
+ */
+public class SHA1 {
+
+	public byte[] getMessageDigest(byte[] message) {
+		try {
+			MessageDigest sha = MessageDigest.getInstance("SHA-1");
+			sha.update(message);
+			return sha.digest();
+		} catch (Exception e) {
+			System.err.println("SHA1 Calculation error: " + e);
+		}
+		return null;
+	}
+}
