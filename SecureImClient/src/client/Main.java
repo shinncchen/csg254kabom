ee9491f70b40235ec0d615208505a728cd1aee55
raghuramcbz <raghuramcbz@f2bf0144-0f3c-11de-9004-1fff2a6de87b>
2009-04-10 03:09:42 +0000

    Custom arguments to start client

--- SecureImClient/src/client/Main.java
+++ SecureImClient/src/client/Main.java
@@ -8,13 +8,28 @@ import client.gui.ClientIM;
  */
 
 /**
- *
- * @author Raghuram
+ * To start the client using default settings, just call main without arguments.
+ * To start the client with custom settings, use: 
+ * arg1 - Server IP
+ * arg2 - Server Port
+ * arg3 - Local Port
  */
 public class Main {
 
     public static void main(String[] args) {
-
+        
+        if(args != null && args.length == 3) {
+            try {
+                ChatMaster.SERVER_IP = args[0];
+                ChatMaster.SERVER_PORT = Integer.parseInt(args[1]);
+                ChatMaster.LOCAL_PORT = Integer.parseInt(args[2]);
+            } catch (Exception e) {
+                System.out.println("Invalid network details.");
+                e.printStackTrace();
+                System.exit(0);
+            }
+        }
+        
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new ClientIM().setVisible(true);
